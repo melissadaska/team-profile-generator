89ddff4d72f70f5480eff70a9d47b0e85ba2345d
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-05-22 21:56:40 -0500

    add engineer tests and class that extends employee to make tests pass

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,19 @@
+const Engineer = require("../lib/Engineer")
+
+test('Create GitHub account', () => {
+    const testValue = "GitHubUser";
+    const member = new Engineer("Dean", 1, "email@test.com", testValue);
+    expect(member.github).toBe(testValue);
+});
+
+test("getRole() to return \"Engineer\"", () => {
+    const testValue = "Engineer";
+    const member = new Engineer("Dean", 1, "email@test.com", "GitHubUser");
+    expect(member.getRole()).toBe(testValue);
+});
+
+test("getGithub() to return GitHub username", () => {
+    const testValue = "GitHubUser";
+    const member = new Engineer("Dean", 1, "email@test.com", testValue);
+    expect(member.getGithub()).toBe(testValue);
+});
