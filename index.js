3137d820a4e8b019372bb24257d09519fb344589
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-05-23 20:52:01 -0500

    add  icons to html

--- index.js
+++ index.js
@@ -97,6 +97,7 @@ function initializeHTML() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"/>
         <title>Team Profile</title>
         <style>
             .row {
@@ -158,6 +159,7 @@ function addHTML(member) {
                 <h4>${name}</h4>
             </div>
             <div class="col card-header">
+                <i class="fas fa-laptop"></i>
                 <h4>${role}</h4>
             </div>
             <ul class="list-group list-group-flush text">
@@ -173,6 +175,7 @@ function addHTML(member) {
                 <h4>${name}</h4>
             </div>
             <div class="col card-header">
+                <i class="fas fa-university"></i>
                 <h4>${role}</h4>
             </div>
             <ul class="list-group list-group-flush text">
@@ -188,6 +191,7 @@ function addHTML(member) {
                 <h4>${name}</h4>
             </div>
             <div class="col card-header">
+                <i class="fas fa-trophy"></i>
                 <h4>${role}</h4>
             </div>
             <ul class="list-group list-group-flush text">
